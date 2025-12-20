1c4d25c7208a7ed1973374685be79c7e1b445509
Lain Soykaf <lain@lain.com>
2025-12-20 14:15:46 +0400

    Security: enforce HTTP response size limit

--- lib/pleroma_redux/http/req.ex
+++ lib/pleroma_redux/http/req.ex
@@ -3,16 +3,55 @@ defmodule PleromaRedux.HTTP.Req do
 
   @default_opts [redirect: false, receive_timeout: 5_000]
   @default_req_options []
+  @default_max_response_bytes 1_000_000
 
   defp req_options do
     Application.get_env(:pleroma_redux, :req_options, @default_req_options)
   end
 
+  defp max_response_bytes do
+    Application.get_env(:pleroma_redux, :http_max_response_bytes, @default_max_response_bytes)
+  end
+
+  defp limited_into_fun(max_bytes) when is_integer(max_bytes) and max_bytes > 0 do
+    fn {:data, chunk}, {req, resp} ->
+      chunk_size = IO.iodata_length(chunk)
+      current = Req.Response.get_private(resp, :pleroma_redux_http_body_bytes, 0)
+      new_size = current + chunk_size
+
+      if new_size > max_bytes do
+        resp =
+          resp
+          |> Req.Response.put_private(:pleroma_redux_http_body_bytes, new_size)
+          |> Map.replace!(:body, {:error, :response_too_large})
+
+        {:halt, {req, resp}}
+      else
+        resp =
+          resp
+          |> Req.Response.put_private(:pleroma_redux_http_body_bytes, new_size)
+          |> Map.replace!(:body, resp.body <> IO.iodata_to_binary(chunk))
+
+        {:cont, {req, resp}}
+      end
+    end
+  end
+
   @impl true
   def get(url, headers) do
-    case Req.get(url, [headers: headers] ++ req_options() ++ @default_opts) do
+    opts =
+      [headers: headers, into: limited_into_fun(max_response_bytes())] ++ req_options() ++
+        @default_opts
+
+    case Req.get(url, opts) do
       {:ok, response} ->
-        {:ok, %{status: response.status, body: response.body, headers: response.headers}}
+        case response.body do
+          {:error, :response_too_large} ->
+            {:error, :response_too_large}
+
+          body ->
+            {:ok, %{status: response.status, body: body, headers: response.headers}}
+        end
 
       {:error, _} = error ->
         error
@@ -21,9 +60,19 @@ defmodule PleromaRedux.HTTP.Req do
 
   @impl true
   def post(url, body, headers) do
-    case Req.post(url, [body: body, headers: headers] ++ req_options() ++ @default_opts) do
+    opts =
+      [body: body, headers: headers, into: limited_into_fun(max_response_bytes())] ++
+        req_options() ++ @default_opts
+
+    case Req.post(url, opts) do
       {:ok, response} ->
-        {:ok, %{status: response.status, body: response.body, headers: response.headers}}
+        case response.body do
+          {:error, :response_too_large} ->
+            {:error, :response_too_large}
+
+          response_body ->
+            {:ok, %{status: response.status, body: response_body, headers: response.headers}}
+        end
 
       {:error, _} = error ->
         error
