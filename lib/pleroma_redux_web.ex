c6ad4c619beede994e78c21a405f5ad8a4ecffe9
Lain Soykaf <lain@lain.com>
2025-12-22 02:03:09 +0400

    UI: extract shared composer form

--- lib/pleroma_redux_web.ex
+++ lib/pleroma_redux_web.ex
@@ -90,6 +90,7 @@ defmodule PleromaReduxWeb do
       # Common modules used in templates
       alias Phoenix.LiveView.JS
       alias PleromaReduxWeb.AppShell
+      alias PleromaReduxWeb.Composer
       alias PleromaReduxWeb.Layouts
       alias PleromaReduxWeb.MediaViewer
       alias PleromaReduxWeb.StatusCard
