7054c9e1997a357d9455430ec1f5e8d52fffd3f1
Lain Soykaf <lain@lain.com>
2026-01-11 23:49:14 +0400

    Admin: fix LiveDashboard dev route conflict

--- lib/egregoros_web/router.ex
+++ lib/egregoros_web/router.ex
@@ -295,19 +295,12 @@ defmodule EgregorosWeb.Router do
   #   pipe_through :api
   # end
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
+  # Enable Swoosh mailbox preview in development.
+  # LiveDashboard is available at /admin/dashboard.
   if Application.compile_env(:egregoros, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
     scope "/dev" do
       pipe_through :browser
 
-      live_dashboard "/dashboard", metrics: EgregorosWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
